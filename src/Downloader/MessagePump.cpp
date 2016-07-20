8e0573d19c6a58f75151ff9be123c699f18c0a52
kevin <janyboo@gmail.com>
2016-07-20 11:00:47 +0800

    完成通知机制 1.增加公用通知类型定义头文件 2.实现通知转回调代码

--- src/Downloader/MessagePump.cpp
+++ src/Downloader/MessagePump.cpp
@@ -3,10 +3,12 @@
 #include <Poco/Thread.h>
 #include <Poco/Notification.h>
 #include <Poco/NotificationQueue.h>
+#include <Poco/NotificationCenter.h>
 
 using Poco::Thread;
 using Poco::Notification;
 using Poco::NotificationQueue;
+using Poco::NotificationCenter;
 
 MessagePump::MessagePump()
 :_pump(this, &MessagePump::runActivity)
@@ -29,19 +31,23 @@ void MessagePump::stop()
 	_pump.wait();
 }
 
+/*
+runActivity can't dispatch download thread's notification,
+because download notification will be freq.
+*/
+
 void MessagePump::runActivity()
 {
+	NotificationQueue& queue = NotificationQueue::defaultQueue();
+	NotificationCenter& nc = NotificationCenter::defaultCenter();
+
 	while (!_pump.isStopped())
 	{
-		NotificationQueue& queue = NotificationQueue::defaultQueue();
 		Notification::Ptr pNf = queue.waitDequeueNotification();
-		if (pNf)
+		
+		if (pNf && nc.hasObservers())
 		{
-			/*WorkNotification::Ptr pWorkNf = pNf.cast<WorkNotification>();
-			if (pWorkNf)
-			{
-				nc.notify();
-			}*/
+			nc.postNotification(pNf);
 		}
 		else Thread::sleep(200);
 	}
